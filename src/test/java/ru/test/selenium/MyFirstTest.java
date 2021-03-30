6d1676222508e2f9259e1e0d48e9beed8def195c
Скобкарева Екатерина Вадимовна (4040393) <skobkareva@dev.vtb.ru>
2021-03-30 22:26:46 +0400

    add test

--- src/test/java/ru/test/selenium/MyFirstTest.java
+++ src/test/java/ru/test/selenium/MyFirstTest.java
@@ -1,10 +1,10 @@
 package ru.test.selenium;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MyFirstTest {
@@ -14,18 +14,23 @@ public class MyFirstTest {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver,10);
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void myFirstTest(){
-
+    public void myFirstTest() {
+        driver.get("https://market.yandex.ru/");
+        driver.findElement(By.xpath("//div[@data-zone-name='category-link']//span[text()='Электроника']/parent::a")).click();
+        Assert.assertEquals("Электроника", driver.findElement(By.xpath("//h1")).getText());
     }
 
     @After
-    public void stop(){
+    public void stop() {
         driver.quit();
-        driver=null;
+        driver = null;
     }
 }
