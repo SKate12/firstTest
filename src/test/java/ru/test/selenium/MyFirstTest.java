f8b96403f20d13516c744437d3c7ddb361cdae7a
Скобкарева Екатерина Вадимовна (4040393) <skobkareva@dev.vtb.ru>
2021-03-30 20:58:46 +0400

    delete

--- src/test/java/ru/test/selenium/MyFirstTest.java
+++ src/test/java/ru/test/selenium/MyFirstTest.java
@@ -3,7 +3,6 @@ package ru.test.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
