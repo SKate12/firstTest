eb8636f71694447fede87da627c73e79370716b9
Скобкарева Екатерина Вадимовна (4040393) <skobkareva@dev.vtb.ru>
2021-03-31 13:55:03 +0400

    add LitecartAdminLoginTest

---
+++ src/test/java/ru/test/selenium/LitecartAdminLoginTest.java
@@ -0,0 +1,41 @@
+package ru.test.selenium;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LitecartAdminLoginTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void litecartAdminLoginTest() {
+        driver.get("http://127.0.0.1/litecart/admin");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@type='submit' and @name='login']")).click();
+        Assert.assertTrue(driver.findElement(By.id("sidebar")).isDisplayed());
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
